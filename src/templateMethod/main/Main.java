801b30c1eca9f72152d3ec656ef9447f81ab7e00
JuanJosÃeCifuentes <jcifuentesc03@gmail.com>
2023-08-16 11:12:09 -0500

    Agrega funcion de inventario dinamico a template method

--- src/templateMethod/main/Main.java
+++ src/templateMethod/main/Main.java
@@ -10,7 +10,12 @@ public class Main {
     public static void main(String[] args) {
 
         //Define el stock de la siguiente forma: {Electronica, Ropa, Alimento}
-        int[] cantidadesInventario = {3, 2, 3};
+        //Hay un 20% de probabilidad de que el producto se haya agotado.
+        int cantidadElectronica, cantidadRopa, cantidadAlimento;
+        cantidadElectronica = (Math.random() > 0.2) ? 5 : 0;
+        cantidadRopa = (Math.random() > 0.2) ? 5 : 0;
+        cantidadAlimento = (Math.random() > 0.2) ? 5 : 0;
+        int[] cantidadesInventario = {cantidadElectronica, cantidadRopa, cantidadAlimento};
         Inventario.setInventario(cantidadesInventario);
 
         Usuario cliente = new Usuario("Juanjo", "juancicu@unisabana.edu.co");
